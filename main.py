cff67476eb400637d683c41ea97171155f80af24
DaleNaci <dale.nash01@gmail.com>
2018-08-14 00:03:52 -0400

    Created ACTUALY main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import scores, csv
+if __name__ == '__main__':
+  with open('problems.csv') as probfile:
+    with open('users.csv') as usefile:
+      problems = csv.reader(probfile)
+      names = csv.reader(usefile)
+      print(score_dict(names, problems))
