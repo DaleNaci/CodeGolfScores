4863cef30b67e36c5943d9544045a792f77873c5
DaleNaci <dale.nash01@gmail.com>
2018-08-13 23:47:53 -0400

    Fixed tabs and spaces error

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def score_dict(names, problems):#Where names is a list containing usernames and 
 		scores={}
 		try:
 			r = requests.get('https://code-golf.io/scores/' + prob + '/python')
-      			html = BeautifulSoup(r.text, 'html.parser')
-    		except:
-      			print('Uwu We made a fucky wucky! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HARD to fix this!')
+      html = BeautifulSoup(r.text, 'html.parser')
+    except:
+      print('Uwu We made a fucky wucky! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HARD to fix this!')
 		for row in html.find_all('tr'):
-      			scores[row.td.a.contents[0].chlidren[0]] = row.find_all('td')[2].contents[0].children[0]
-    		for name in names:
-      			bad_chars='()'
+      scores[row.td.a.contents[0].chlidren[0]] = row.find_all('td')[2].contents[0].children[0]
+    	for name in names:
+      		bad_chars='()'
       		try:
         		users[name] += int(("".join(c for c in score[name] if c not in bad_chars) - ' points'))
       		except:
