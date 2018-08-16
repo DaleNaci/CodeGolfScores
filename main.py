823f72a66a73e8baf79a2de1013a44d82cebc96d
Peter <plande@crprairie.org>
2018-08-16 11:01:47 -0500

    Fixed code and reorganized files

--- main.py
+++
@@ -1,9 +0,0 @@
-import scores, csv
-if __name__ == '__main__':
-  with open('problems.csv') as probfile:
-    with open('users.csv',) as usefile:
-      problemsReader = csv.reader(probfile)
-      problems = list(problemsReader)
-      namesReader = csv.reader(usefile)
-      names = list(namesReader)
-      print(scores.score_dict(names, problems))
