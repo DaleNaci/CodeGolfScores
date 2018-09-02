702a092f37536cdcebafb35ddf9ad25a56251d37
DaleNaci <dale.nash01@gmail.com>
2018-09-02 16:54:41 -0400

    Messed around more with the syntax and logic

--- scores.py
+++ scores.py
@@ -10,20 +10,27 @@ r = requests.get('https://code-golf.io/scores')
 if not r.status_code == 200:
 	raise ConnectionError
 
-html = soup(r.text, 'html.parser')
+# html = soup(r.text, 'html.parser')
 
-outer = html.main.table.tr
 
-print(outer.td.findNext('td').a.contents[0])
-print(outer.td.findNext('td').findNext('td').contents[0])
+# outer = html.main.table.tr
 
-print(outer.td.findNext('td').a.contents[0])
-print(outer.td.findNext('td').findNext('td').contents[0])
+# print(outer.td.findNext('td').a.contents[0])
+# print(outer.td.findNext('td').findNext('td').contents[0])
+
+# print(outer.td.findNext('td').a.contents[0])
+# print(outer.td.findNext('td').findNext('td').contents[0])
 
 # for i in range(1):
 # 	outer = html.main.table.tr
 # 	for j in range(i):
 		
+html = soup(r.text, 'html.parser')
+table = html.findAll("table")[0]
+rows = table.findAll('tr')
+for tr in rows:
+	print(tr.get_text(strip=True))
+
 
 
 
