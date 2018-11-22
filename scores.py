80e13e504492455f62c33d4e903898d205f9cc04
DaleNaci <dale.nash01@gmail.com>
2018-11-22 12:23:06 -0500

    added functionality

--- scores.py
+++ scores.py
@@ -1,44 +1,24 @@
 import requests
 from bs4 import BeautifulSoup as soup
 
-class ConnectionError(Exception):
-	def __init__(self):
-		self.message = 'Connection failed'
-		super().__init__(self)
+users = open("example/users.txt", "r").read().split(",")
+users.pop(len(users)-1)
 
-r = requests.get('https://code-golf.io/scores')
-if not r.status_code == 200:
-	raise ConnectionError
+points = []
 
-html = soup(r.text, 'html.parser')
+for u in users:
+	r = requests.get("https://code-golf.io/users/" + u)
+	html = soup(r.text, "html.parser")
+	points.append(int(html.table.tr.td.contents[0]))
 
+for i in range(len(points)):
+	m = i
+	for j in range(i+1, len(points)):
+		if points[m] < points[j]:
+			m = j
+	points[i], points[m] = points[m], points[i]
+	users[i], users[m] = users[m], users[i]
 
-outer = html.main.table.tr
-
-print(outer.td.findNext('td').a.contents[0])
-print(outer.td.findNext('td').findNext('td').contents[0])
-
-# print(outer.td.findNext('td').a.contents[0])
-# print(outer.td.findNext('td').findNext('td').contents[0])
-
-# for i in range(1):
-# 	outer = html.main.table.tr
-# 	for j in range(i):
-		
-# html = soup(r.text, 'html.parser')
-# table = html.findAll("table")[0]
-# rows = table.findAll('tr')
-#for tr in rows:
-	
-
-
-
-
-# OLD
-
-#USER = ENTIRE "tr" SECTION
-# userList = html.find_all("tr")
-
-# print(userList[0].text)
-
-#for user in userList:
+for i in range(0, len(points)):
+	print(users[i] + ": "),
+	print points[i]
